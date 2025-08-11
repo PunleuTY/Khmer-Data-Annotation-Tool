d236805f51639e992019760b10c73ea067bc23fa
Sievlinh <sievlinhk@gmail,com>
2025-08-11 20:06:22 +0700

    navbar

--- frontend/src/pages/Upload.jsx
+++ frontend/src/pages/Upload.jsx
@@ -1,8 +1,11 @@
 const Upload = () => {
   
   return (
-    <div className="min-h-full bg-gray-50">
-      <h1 className="text-2xl font-bold">Upload Page</h1>
+    <div className="min-h-full bg-gray-50 m-6">
+      <h1 className="text-3xl font-bold">Annotate</h1>
+      <div className="bg-[#E5E9EC] px-2 py-1 my-3 rounded inline-block w-fit">
+        <h4 className="text-sm font-semibold">Tip: Use keyboard shortcuts</h4>
+      </div>
     </div>
   );
 };
